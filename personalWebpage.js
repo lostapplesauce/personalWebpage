64bbf5007c676fc73311c464861b0ec5c9899973
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2021-09-26 22:51:23 -0700

    added changes

--- personalWebpage.js
+++ personalWebpage.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+
+var color = document.querySelector('.color');
+
+// function bgColor(color){
+//    document.body.style.backgroundColor = color; 
+// }
+
+color.addEventListener('click',function(){
+    
+   document.body.classList.toggle("colorred");
+     
+});
+
+
