4c65e3da637ac9885a4e6a0ed5891e1a395cd628
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2021-12-14 05:03:43 -0800

    added more updates

--- personalWebpage.js
+++ personalWebpage.js
@@ -1,6 +1,6 @@
 // JavaScript source code
 
-var color = document.querySelector('.color');
+var color = document.querySelector('#color');
 
 // function bgColor(color){
 //    document.body.style.backgroundColor = color; 
@@ -11,5 +11,26 @@ color.addEventListener('click', function () {
    document.body.classList.toggle("colorred");
 
 });
+
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
 
 
