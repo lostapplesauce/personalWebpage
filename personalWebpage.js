46b17a64378b5e57dff99be83982efab18294ef1
lostapplesauce <48301604+lostapplesauce@users.noreply.github.com>
2021-12-20 11:50:04 -0800

    Updated menu

--- personalWebpage.js
+++ personalWebpage.js
@@ -21,7 +21,7 @@ hamburger.addEventListener("click", mobileMenu);
 function mobileMenu() {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-}
+} 
 
 const navLink = document.querySelectorAll(".nav-link");
 
